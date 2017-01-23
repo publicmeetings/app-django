40de023a5128f7772d66127bfc3fb018d9214918
Lev Tatz <lev.tatz@gmail.com>
2017-01-22 18:17:58 -0800

    add messages

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
+from django.contrib import messages
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic.edit import CreateView
 
 from forms import MeetingForm
@@ -8,3 +10,7 @@ from models import Meeting
 class MeetingNew(CreateView):
     model = Meeting
     form_class = MeetingForm
+
+    def get_success_url(self):
+        messages.success(self.request, 'Meeting saved.')
+        return reverse('meeting-new')
