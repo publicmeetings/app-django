74d2a81382dc6d78440fc6fad822b52316495316
Lev Tatz <lev.tatz@gmail.com>
2017-01-22 17:33:27 -0800

    meeting model and migration

--- core/models.py
+++ core/models.py
@@ -2,4 +2,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Meeting(BaseModel):
+    title = models.CharField(max_length=255)
+    topic = models.TextField()
+    link = models.URLField()
+    datetime = models.DateTimeField()
+
+    # tmp
+    organization = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
