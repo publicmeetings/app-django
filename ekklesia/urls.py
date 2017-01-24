d8b0083e3442d597ccfab2c5705955283766bffe
Lev Tatz <lev.tatz@gmail.com>
2017-01-23 19:23:07 -0500

    homepage

--- ekklesia/urls.py
+++ ekklesia/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^', include('core.urls')),
     url(r'^admin/', admin.site.urls),
 ]
