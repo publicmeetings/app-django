9d8f3aa7c11fe86783b0ab5a494081570c52e741
Lev Tatz <lev.tatz@gmail.com>
2017-01-29 14:09:56 -0500

    make meeting list home

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ from views import Meetings, MeetingNew
 
 
 urlpatterns = [
-    url(r'^meetings/$', Meetings.as_view(), name='meetings'),
-    url(r'^meetings/new/$', MeetingNew.as_view(), name='meeting-new'),
+    url(r'^$', Meetings.as_view(), name='meetings'),
+    url(r'^new/$', MeetingNew.as_view(), name='meeting-new'),
 ]
--- publicmeetings/urls.py
+++ publicmeetings/urls.py
@@ -19,7 +19,6 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^', include('core.urls')),
     url(r'^admin/', admin.site.urls),
 ]
